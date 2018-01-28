317d51755e3ef63655f063c59e784b61b6e81ea0
Rescue9 <rescue@corridor9.net>
2018-01-28 11:07:56 -0600

    Started Restaurant Tip app. Finished Main & Second activity ui

---
+++ Chapter3RestaurantTip/app/src/main/java/com/example/rescue/chapter3restauranttip/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.rescue.chapter3restauranttip;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    /*
+     setup variables to hide UI
+     */
+    TextView calculation_totals;
+    LinearLayout totals_text;
+    LinearLayout totals_amt;
+
+    EditText meal_cost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+         associate objects with variables
+         */
+        calculation_totals = (TextView) findViewById(R.id.textCaluclationTotals);
+        totals_text = (LinearLayout) findViewById(R.id.layoutTotalsTxt);
+        totals_amt = (LinearLayout) findViewById(R.id.layoutTotalsNum);
+        meal_cost = (EditText) findViewById(R.id.editMealCost);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent("com.example.rescue.chapter3restauranttip.SecondActivity");
+
+        /*
+         bundle meal cost into intent
+         */
+        Bundle extras = new Bundle();
+        extras.putString("meal_cost", meal_cost.getText().toString());
+
+        intent.putExtras(extras);
+
+        startActivityForResult(intent, 1);
+
+    }
+}
