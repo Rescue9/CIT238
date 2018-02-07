b23cbe2f27f03d073f536ea056f5d45a95d4c768
Rescue9 <rescue@corridor9.net>
2018-02-07 16:46:35 -0600

    Added comments and generated javadoc for whole project

--- Week5Souvenirs/app/src/main/java/com/example/rescue/week5souvenirs/MainActivity.java
+++ Week5Souvenirs/app/src/main/java/com/example/rescue/week5souvenirs/MainActivity.java
@@ -1,4 +1,11 @@
 package com.example.rescue.week5souvenirs;
+/**
+ * Author: Andrew Buskov
+ * Date: 2/7/2018
+ * Class: CIT 238 (Android)
+ * Purpose: To create an application that allows the sale of souvenirs
+ *   during the 2018 Olympic Games
+ */
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +22,7 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* instantiate variables to be used throughout the class */
     EditText firstName;
     EditText lastName;
     CheckBox checkBox1;
@@ -32,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
     final double costEachItem = 31.95;
     DecimalFormat currency = new DecimalFormat("$###,###.00");
 
+    /**
+     * Create the instance and declare variables
+     * @param savedInstanceState Used for saving data should the instance need to be
+     *                           recreated. For Ex.; during orientation changes
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +65,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Method is called when the Clear button is clicked
+     * All views are reset to default values
+     * @param view
+     */
     public void onClickClear(View view) {
         firstName.setText("");
         lastName.setText("");
@@ -65,12 +83,18 @@ public class MainActivity extends AppCompatActivity {
         output.setText("");
     }
 
+    /**
+     * Method is called when the Submit button is clicked
+     * Various error checking is performed
+     * Output information, or receipt, is sent to TextView
+     * @param view
+     */
     public void onClickSubmit(View view) {
         String wholeName;
         String last4;
         itemsSelected = "";
 
-        // make sure the first and last name have more than 1 character
+        /* make sure the first and last name have more than 1 character */
         if (firstNameLength() && lastNameLength()){
             wholeName = firstName.getText() + " " + lastName.getText();
         } else {
@@ -79,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        // make sure the card number contains exactly 16 digits
+        /* make sure the card number contains exactly 16 digits */
         if (cardNumberLength()) {
             last4 = TextUtils.substring(cardNumber.getText().toString(), cardNumber.length()-4, cardNumber.length());
         } else {
@@ -88,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        // make sure that at least one checkbox is checked and format string for toys purchased
+        /* make sure that at least one checkbox is checked and format string for toys purchased */
         if (checkCheckBoxes() == 0){
             Toast.makeText(this, "You must select at least one item with checkbox", Toast.LENGTH_LONG).show();
             output.setText("");
@@ -125,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // make output string for information box
+        /* make output string for information box */
         String outputString = "Receipt for " + wholeName + "\n" +
                 "Purchased on " + date.getSelectedItem().toString() + "\n" +
                 "Last 4 digits of " + creditCard.getSelectedItem().toString() + " credit card: " +
@@ -135,7 +159,10 @@ public class MainActivity extends AppCompatActivity {
         output.setText(outputString);
     }
 
-    // check length of first name
+    /**
+     * Method checks to make sure first name is longer than two characters
+     * @return true or false
+     */
     public boolean firstNameLength() {
         if (firstName.length() < 2) {
             return false;
@@ -144,7 +171,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // check length of last name
+    /**
+     * Method checks to make sure last name is longer than two characters
+     * @return true or false
+     */
     public boolean lastNameLength() {
         if (lastName.length() < 2) {
             return false;
@@ -153,7 +183,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // check length of credit card
+    /**
+     * Method checks to make sure that credit card number is exactly 16 numbers in length
+     * @return true or false
+     */
     public boolean cardNumberLength() {
         if (cardNumber.length() == 16) {
             return true;
@@ -162,7 +195,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // return total number of checked boxes
+    /**
+     * Method calculates the number of checkboxes that are checked
+     * @return number of checked checkboxes
+     */
     public int checkCheckBoxes() {
         int checked = 0;
         if (checkBox1.isChecked())
@@ -177,7 +213,12 @@ public class MainActivity extends AppCompatActivity {
         return checked;
     }
 
-    // return total cost of order
+    /**
+     *Method calculates the total cost of the order by multiplying the total checked boxes *
+     * the cost of each item
+     * @param totalBoxesChecked the number of items checked
+     * @return the total cost for the order
+     */
     public double totalCost(int totalBoxesChecked) {
         return costEachItem * totalBoxesChecked;
     }
