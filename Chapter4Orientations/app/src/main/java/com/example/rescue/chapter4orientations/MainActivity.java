e70878f9b8285c5e96761777c3668b513ca10ccd
Rescue9 <rescue@corridor9.net>
2018-02-13 16:23:37 -0600

    completed Chapter4 exercises

---
+++ Chapter4Orientations/app/src/main/java/com/example/rescue/chapter4orientations/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.rescue.chapter4orientations;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("StateInfo", "onCreate");
+    }
+
+    @Override
+    public void onStart() {
+        Log.d("StateInfo", "onStart");
+        super.onStart();
+    }
+    @Override
+    public void onResume() {
+        Log.d("StateInfo", "onResume");
+        super.onResume();
+    }
+    @Override
+    public void onPause() {
+        Log.d("StateInfo", "onPause");
+        super.onPause();
+    }
+    @Override
+    public void onStop() {
+        Log.d("StateInfo", "onStop");
+        super.onStop();
+    }
+    @Override
+    public void onDestroy() {
+        Log.d("StateInfo", "onDestroy");
+        super.onDestroy();
+    }
+    @Override
+    public void onRestart() {
+        Log.d("StateInfo", "onRestart");
+        super.onRestart();
+    }
+}
