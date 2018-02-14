fd24159e076e0545b632f9c12d7a73f43720bad2
Rescue9 <rescue@corridor9.net>
2018-02-14 10:35:16 -0600

    modified layouts

--- Week6SterlingSilver/app/src/main/java/com/example/rescue/week6sterlingsilver/MainActivity.java
+++ Week6SterlingSilver/app/src/main/java/com/example/rescue/week6sterlingsilver/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        
     }
 
     @Override
