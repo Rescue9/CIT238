1e99f1ba2ee7f9393aa7143f0a10824b940f6000
Rescue9 <andrew.buskov@gmail.com>
2018-02-22 14:27:28 -0600

    playing with datepicker

--- Chapter5BasicViews4/app/src/main/java/com/example/rescue/chapter5basicviews4/MainActivity.java
+++ Chapter5BasicViews4/app/src/main/java/com/example/rescue/chapter5basicviews4/MainActivity.java
@@ -4,28 +4,52 @@ import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    TimePickerDialog.OnTimeSetListener dialogListener = new TimePickerDialog
-            .OnTimeSetListener() {
-        @Override
-        public void onTimeSet(TimePicker timePicker, int i, int i1) {
-            Log.d("int1", String.valueOf(i));
-            Log.d("int2", String.valueOf(i1));
-        }
-    };
-    Calendar cal = Calendar.getInstance();
+    TimePicker timePicker;
+    DatePicker datePicker;
+    int hour, minute;
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showTimeDialog();
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
     }
-    public void showTimeDialog(){
-        new TimePickerDialog(MainActivity.this,dialogListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false).show(); {};
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+            new TimePickerDialog.OnTimeSetListener()
+            {
+                public void onTimeSet(
+                        TimePicker view, int hourOfDay, int minuteOfHour)
+                {
+                    hour = hourOfDay;
+                    minute = minuteOfHour;
+                    SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm aa");
+                    Date date = new Date();
+                    String strDate = timeFormat.format(date);
+                    Log.d("Got Here", "yes");
+                    Toast.makeText(getBaseContext(),
+                            "You have selected " + strDate,
+                            Toast.LENGTH_LONG).show();
+                }
+            };
+    public void onClick(View view) {
+        Toast.makeText(getBaseContext(),
+                "Date selected:" + (datePicker.getMonth() + 1) +
+                        "/" + datePicker.getDayOfMonth() +
+                        "/" + datePicker.getYear() + "\n" +
+                        "Time selected:" + timePicker.getHour() +
+                        ":" + timePicker.getMinute(),
+                Toast.LENGTH_SHORT).show();
     }
 }
