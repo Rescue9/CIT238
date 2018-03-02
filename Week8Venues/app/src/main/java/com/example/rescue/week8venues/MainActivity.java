5d679730ecaeb33c5d65aee25906773e3cfcbfba
Rescue9 <andrew.buskov@gmail.com>
2018-03-01 21:18:39 -0600

    removed toast

--- Week8Venues/app/src/main/java/com/example/rescue/week8venues/MainActivity.java
+++ Week8Venues/app/src/main/java/com/example/rescue/week8venues/MainActivity.java
@@ -18,7 +18,6 @@ import android.widget.GridView;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
-import android.widget.Toast;
 import android.widget.ViewSwitcher.ViewFactory;
 
 public class MainActivity extends AppCompatActivity {
@@ -141,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         super.onOptionsItemSelected(item);
 
-        Toast.makeText(this, "Selected: " + item, Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "Selected: " + item, Toast.LENGTH_LONG).show();
         cafeName = item.toString();
 
         // create switch for different cafes //
